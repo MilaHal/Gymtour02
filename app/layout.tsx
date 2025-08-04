507e9be27e93b1fd44a3b8e785855a54535625da
MilaHal <corvetamil@gmail.com>
2025-08-04 23:15:11 +0200

    Update layout.tsx
    Update meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,10 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: {
     default: "Virtuální fitness trenér | GYMTOUR",
     template: "%s | GYMTOUR",
   },
-  description: "Virtuální prohlídka Vaší posilovny, která opravdu zaujme!",
+  description: "Virtuální prohlídka Vaší posilovny, která opravdu zaujme.",
   type: "website",
   locale: "cs-CZ",
 };
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="cs" className={inter.className}>
-      <head />
-      <body>{children}</body>
-    </html>
-  );
-}
 
